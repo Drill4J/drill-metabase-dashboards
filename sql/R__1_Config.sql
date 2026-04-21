0639d267b39b533c36fcc331a120179b9d159bed
iryabov <Igor_Riabov@epam.com>
2026-04-21 08:34:24 +0200

    feat: update environment variables for Metabase configuration and adjust SQL settings

--- sql/R__1_Config.sql
+++ sql/R__1_Config.sql
@@ -1,6 +1,6 @@
 -- This SQL file is used to configure the Drill4J Connection in Metabase.
 UPDATE public.metabase_database SET details = '{"ssl":##{{drillDBSSL}},"password":"##{{drillDBPassword}}","port":##{{drillDBPort}},"advanced-options":false,"schema-filters-type":"all","dbname":"##{{drillDBName}}","host":"##{{drillDBHost}}","tunnel-enabled":false,"user":"##{{drillDBUser}}"}' WHERE name = 'Drill4J_PostgreSQL_DB';
-UPDATE public.setting SET value = '##{{metabaseBaseURL}}' WHERE key = 'site-url';
+UPDATE public.setting SET value = '##{{metricsUiBaseURL}}' WHERE key = 'site-url';
 
 UPDATE public.setting
 SET value = value || ',' || chr(10) || replace(replace('##{{drillUIBaseURL}}', 'https://', ''), 'http://', '')
